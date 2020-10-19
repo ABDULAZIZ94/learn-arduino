9a64ab1d161e874eca8116826484e9cebdb191f9
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-10-19 13:11:17 +0800

    added new 4 channel relay control

--- chain-block/chain-block.ino
+++ chain-block/chain-block.ino
@@ -14,10 +14,11 @@ uint32_t rc_start[RC_NUM_CHANNELS];
 volatile uint16_t rc_shared[RC_NUM_CHANNELS];
 volatile bool need_neutral;
 
+//to make signal not determine
 volatile bool relay1 = false;
 volatile bool relay2 = false;
-volatile bool relay3;
-volatile bool relay4;
+volatile bool relay3 = false;
+volatile bool relay4 = false;
 
 void rc_read_values() {
   noInterrupts();
@@ -25,7 +26,8 @@ void rc_read_values() {
   interrupts();
 }
 
-set_relay_state(){
+void set_relay_state(){
+  
   if(relay1){
     pinMode(4, OUTPUT);
     digitalWrite(4, LOW);
@@ -71,45 +73,15 @@ void calc_input(uint8_t channel, uint8_t input_pin) {
 
 void calc_ch2() { 
   calc_input(RC_CH2, RC_CH2_INPUT); 
-  if(rc_values[RC_CH2] > 1600 && need_neutral == false){
-    volatile unsigned long elapsed_time = 0;
-    volatile unsigned long currrent_time = 0;
-    volatile unsigned long previous_time = 0;
-    while(1){
-      currrent_time = millis();
-      pinMode(6, OUTPUT);
-      pinMode(7, OUTPUT);
-      digitalWrite(6, LOW);
-      digitalWrite(7, LOW);
-      previous_time = currrent_time;
-      currrent_time = millis();
-      elapsed_time = currrent_time - previous_time;
-      if((elapsed_time < 10)){
-        digitalWrite(6, HIGH);
-        digitalWrite(7, HIGH);
-        need_neutral = true;
-        break;
-      }
-    }
-    
-  }else if(rc_values[RC_CH2] < 1400 && need_neutral == false){
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    need_neutral = true;
+  if(rc_values[RC_CH2] > 1700 ){
+    pinMode(4,OUTPUT);
+    digitalWrite(4, LOW);                                                            
+  }else if(rc_values[RC_CH2] < 1300 ){
+    pinMode(6,OUTPUT);
+    digitalWrite(6, LOW);    
   }else{
-    pinMode(6, INPUT);
-    pinMode(7, INPUT);
-    pinMode(5, INPUT);
-    pinMode(4, INPUT);
     digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
     digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    need_neutral = false;
   }
 }
 void calc_ch5() { calc_input(RC_CH5, RC_CH5_INPUT); }
@@ -119,14 +91,14 @@ void setup() {
   Serial.begin(9600);
   pinMode(RC_CH2_INPUT, INPUT);//set analog pin as input pin
   pinMode(RC_CH5_INPUT, INPUT);//set analog pin as input pin
-
+                                              
   enableInterrupt(RC_CH2_INPUT, calc_ch2, CHANGE);//execute function when interrupted by signal from analog pin
   enableInterrupt(RC_CH5_INPUT, calc_ch5, CHANGE);//execute function when interrupted by signal from analog pin
 }
 
 void loop() {
   rc_read_values();
-  set_relay_state();
-//  Serial.print("CH2:"); Serial.print(rc_values[RC_CH2]); Serial.print("\t");
-//  Serial.print("CH5:"); Serial.println(rc_values[RC_CH5]);
+  
+  Serial.print("CH2:"); Serial.print(rc_values[RC_CH2]); Serial.print("\t");
+  Serial.print("CH5:"); Serial.println(rc_values[RC_CH5]);
 }
