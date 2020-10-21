0e42aa29bf67b596b8bd29e29ffea20956caa622
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-10-21 14:49:23 +0800

    upgraded code chain block

--- chain-block/chain-block.ino
+++ chain-block/chain-block.ino
@@ -28,6 +28,9 @@ volatile unsigned long up_elapsed_time = 0;
 volatile unsigned long down_start_time = 0;
 volatile unsigned long down_current_time = 0;
 volatile unsigned long down_elapsed_time = 0;
+volatile unsigned long neutral_start_time = 0;
+volatile unsigned long neutral_current_time = 0;
+volatile unsigned long neutral_elapsed_time = 0;
 
 volatile unsigned long on_duration = 250;
 
@@ -83,25 +86,28 @@ void calc_input(uint8_t channel, uint8_t input_pin) {
 
 void calc_ch2() { 
   calc_input(RC_CH2, RC_CH2_INPUT); 
+  neutral_current_time = millis();
+  neutral_elapsed_time = neutral_current_time - neutral_start_time;
 
-  //calculate how long hoist switched on
-  if( relay1==false && relay2==false ){
-    up_start_time=millis();
-    up_need_neutral = false;
-  }else if( relay1==true && relay2==true ){
-      up_current_time=millis();
-      up_elapsed_time=up_current_time - up_start_time;
-  }
-
-  if( relay3==false && relay4==false ){
-    down_start_time=millis();
-    down_need_neutral = false;
-  }else if( relay3==true && relay4==true ){
-      down_current_time=millis();
-      down_elapsed_time=down_current_time - down_start_time;
-  }
-
+  if(neutral_elapsed_time > 250){
+      //calculate how long hoist switched on
+    if( relay1==false && relay2==false ){
+      up_start_time=millis();
+      up_need_neutral = false;
+      
+    }else if( relay1==true && relay2==true ){
+        up_current_time=millis();
+        up_elapsed_time=up_current_time - up_start_time;
+    }
   
+    if( relay3==false && relay4==false ){
+      down_start_time=millis();
+      down_need_neutral = false;
+    }else if( relay3==true && relay4==true ){
+        down_current_time=millis();
+        down_elapsed_time=down_current_time - down_start_time;
+    }
+  }
   
   if(rc_values[RC_CH2] > 1700 && up_need_neutral==false){
       relay1=true;
@@ -113,14 +119,14 @@ void calc_ch2() {
         return;
       }
   }else if(rc_values[RC_CH2] < 1300 && down_need_neutral==false ){
-      relay3=true;
-      relay4=true;
       if(down_elapsed_time >= on_duration){
         relay3=false;
         relay4=false;
         down_need_neutral=true;
         return;
       }
+      relay3=true;
+      relay4=true;
   }else{
       relay1=false;
       relay2=false;
@@ -128,6 +134,7 @@ void calc_ch2() {
       relay4=false;
       up_elapsed_time=0;
       down_elapsed_time=0;
+      neutral_start_time=millis();
   }
 
 }
@@ -147,7 +154,7 @@ void setup() {
   pinMode(RC_CH5_INPUT, INPUT);//set analog pin as input pin
                                           
   enableInterrupt(RC_CH2_INPUT, calc_ch2, CHANGE);//execute function when interrupted by signal from analog pin
-  enableInterrupt(RC_CH5_INPUT, calc_ch5, CHANGE);//execute function when interrupted by signal from analog pin
+//  enableInterrupt(RC_CH5_INPUT, calc_ch5, CHANGE);//execute function when interrupted by signal from analog pin
 }
 
 void loop() {
@@ -156,11 +163,11 @@ void loop() {
 //  Serial.print("UpStart:");Serial.print(up_start_time);Serial.print("\t");
 //  Serial.print("UpCurrent:");Serial.print(up_current_time);Serial.print("\t");
 //  Serial.print("UpElapsed:");Serial.print(up_elapsed_time);Serial.print("\t");
-  Serial.print("DownStart:");Serial.print(down_start_time);Serial.print("\t");
-  Serial.print("DownCurrent:");Serial.print(down_current_time);Serial.print("\t");
-  Serial.print("DownElapsed:");Serial.print(down_elapsed_time);Serial.print("\t");
+//  Serial.print("DownStart:");Serial.print(down_start_time);Serial.print("\t");
+//  Serial.print("DownCurrent:");Serial.print(down_current_time);Serial.print("\t");
+//  Serial.print("DownElapsed:");Serial.print(down_elapsed_time);Serial.print("\t");
 //  Serial.print("Up Need Neutral:");Serial.print(up_need_neutral);Serial.print("\t");
-  Serial.print("Down Need Neutral:");Serial.println(down_need_neutral);Serial.print("\t");
-//  Serial.print("CH2:"); Serial.print(rc_values[RC_CH2]); Serial.print("\t");
-//  Serial.print("CH5:"); Serial.println(rc_values[RC_CH5]);
+//  Serial.print("Down Need Neutral:");Serial.println(down_need_neutral);Serial.print("\t");
+  Serial.print("CH2:"); Serial.print(rc_values[RC_CH2]); Serial.print("\t");
+  Serial.print("CH5:"); Serial.println(rc_values[RC_CH5]);
 }
